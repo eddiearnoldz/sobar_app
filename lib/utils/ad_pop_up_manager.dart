5c31bb1874df42d5cb9f96a2eae5413dbd52561a
eddie arnold <eddiearnoldz@mac.com>
2024-07-12 11:17:36 +0100

    finished styling ad popup

--- lib/utils/ad_pop_up_manager.dart
+++ lib/utils/ad_pop_up_manager.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:hexcolor/hexcolor.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sobar_app/models/adPopUp.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -22,8 +24,8 @@ class AdPopupManager {
 
   Future<void> showAdPopupIfNeeded(BuildContext context, AdPopUp adPopUp) async {
     int openCount = await getOpenCount();
-    if (openCount > 0 && adPopUp.isLive && openCount % 5 == 0) {
-      showAdPopup(context, adPopUp);
+    if (openCount > 0 && adPopUp.isLive) {
+      Future.delayed(const Duration(seconds: 1), () => showAdPopup(context, adPopUp));
     }
     await incrementOpenCount();
   }
@@ -32,93 +34,108 @@ class AdPopupManager {
     showDialog(
       context: context,
       builder: (context) {
-        return AlertDialog(
-          insetPadding: EdgeInsets.all(10),
-          title: Stack(
-            children: [
-              Align(
-                alignment: Alignment.center,
-                child: Text(
-                  adPopUp.adTitle,
-                  style: TextStyle(fontSize: 24, fontFamily: 'Anton'),
-                  textAlign: TextAlign.center,
-                ),
+        return Stack(children: [
+          AlertDialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5), // Set border radius here
+            ),
+            backgroundColor: HexColor('#F8F48F'),
+            insetPadding: EdgeInsets.all(16),
+            contentPadding: EdgeInsets.all(16),
+            title: Align(
+              alignment: Alignment.center,
+              child: Text(
+                adPopUp.adTitle,
+                style: TextStyle(fontSize: 24, fontFamily: 'Anton'),
+                textAlign: TextAlign.center,
               ),
-              Positioned(
-                right: -5.0,
-                top: -5.0,
-                child: GestureDetector(
-                  onTap: () {
+            ),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(4),
+                  child: CachedNetworkImage(
+                    imageUrl: adPopUp.adImage,
+                    placeholder: (context, url) => Container(
+                      decoration: BoxDecoration(color: HexColor('#F8F48F'), borderRadius: BorderRadius.circular(2)),
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.width - 32,
+                    ),
+                    errorWidget: (context, url, error) => const Icon(Icons.error),
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.width - 32,
+                    fadeInDuration: const Duration(milliseconds: 200),
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  adPopUp.adSubheader,
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, height: 1.2),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(adPopUp.adBody, style: TextStyle(fontSize: 12, height: 1.2)),
+              ],
+            ),
+            actions: [
+              if (adPopUp.adButtonText.isNotEmpty)
+                ElevatedButton(
+                  onPressed: () {
+                    if (adPopUp.adUrl.isNotEmpty) {
+                      String url = adPopUp.adUrl;
+                      try {
+                        if (Platform.isAndroid) {
+                          launchUrl(Uri.parse(url), mode: LaunchMode.externalApplication);
+                        } else {
+                          launchUrl(Uri.parse(url));
+                        }
+                      } catch (e) {
+                        print("error: $e");
+                      }
+                    }
                     Navigator.of(context).pop();
                   },
-                  child: Icon(Icons.close, color: Colors.black),
-                ),
-              ),
-            ],
-          ),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(4),
-                child: CachedNetworkImage(
-                  imageUrl: adPopUp.adImage,
-                  placeholder: (context, url) => Container(
-                    decoration: BoxDecoration(color: Theme.of(context).colorScheme.primary, borderRadius: BorderRadius.circular(2)),
-                    width: MediaQuery.of(context).size.width - 20,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Theme.of(context).colorScheme.onPrimary,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                   ),
-                  errorWidget: (context, url, error) => const Icon(Icons.error),
-                  width: MediaQuery.of(context).size.width - 20,
-                  fadeInDuration: const Duration(milliseconds: 200),
-                  fit: BoxFit.contain,
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Text(
-                adPopUp.adSubheader,
-                style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Text(adPopUp.adBody, style: TextStyle(fontSize: 12)),
-            ],
-          ),
-          actions: [
-            if (adPopUp.adButtonText.isNotEmpty)
-              ElevatedButton(
-                onPressed: () {
-                  if (adPopUp.adUrl.isNotEmpty) {
-                    String url = adPopUp.adUrl;
-                    try {
-                      if (Platform.isAndroid) {
-                        launchUrl(Uri.parse(url), mode: LaunchMode.externalApplication);
-                      } else {
-                        launchUrl(Uri.parse(url));
-                      }
-                    } catch (e) {
-                      print("error: $e");
-                    }
-                  }
-                  Navigator.of(context).pop();
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Theme.of(context).colorScheme.onPrimary,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
+                  child: Text(
+                    adPopUp.adButtonText,
+                    style: TextStyle(fontSize: 20, fontFamily: 'Anton', color: HexColor('#F8F48F')),
                   ),
-                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                 ),
-                child: Text(
-                  adPopUp.adButtonText,
-                  style: TextStyle(fontSize: 20, fontFamily: 'Anton', color: Theme.of(context).colorScheme.primary),
+            ],
+            actionsAlignment: MainAxisAlignment.center,
+          ),
+          Positioned(
+            top: 15,
+            right: 5,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+              child: Container(
+                decoration: BoxDecoration(color: HexColor('#F8F48F'), shape: BoxShape.circle, border: Border.all(width: 1, color: Theme.of(context).colorScheme.onPrimary)),
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Icon(
+                    Icons.close,
+                    color: Theme.of(context).colorScheme.onPrimary,
+                    size: 20,
+                  ),
                 ),
               ),
-          ],
-          actionsAlignment: MainAxisAlignment.center,
-        );
+            ),
+          ),
+        ]);
       },
     );
   }
