58c37ab78ce167300613e3a9ddb65f0640232130
eddie arnold <eddiearnoldz@mac.com>
2024-07-18 22:39:59 +0100

    Added stack to filter drinks to allow for gradient bottom transparent transition container

--- lib/components/filtered_drinks_results_list.dart
+++ lib/components/filtered_drinks_results_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 import 'package:sobar_app/models/drink.dart';
 
 class FilterDrinkResultsList extends StatelessWidget {
@@ -10,52 +11,70 @@ class FilterDrinkResultsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .6,
-      child: ListView.builder(
-        scrollDirection: Axis.vertical,
-        itemCount: filteredDrinks.length,
-        itemBuilder: (context, index) {
-          final drink = filteredDrinks[index];
-          return GestureDetector(
-            onTap: () => onDrinkSelected(drink),
-            child: Container(
-              width: MediaQuery.of(context).size.width * 0.75,
-              padding: const EdgeInsets.all(5),
-              margin: const EdgeInsets.only(top: 5, left: 10),
-              decoration: BoxDecoration(
-                color: _getDrinkColor(drink.type),
-                borderRadius: BorderRadius.circular(10),
+    Color gradientColor = isBlackStyle ? HexColor("#212121") : HexColor("#FCF4F0");
+
+    return Stack(alignment: Alignment.bottomCenter, children: [
+      SizedBox(
+        height: MediaQuery.of(context).size.height * .6,
+        child: ListView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: filteredDrinks.length,
+          itemBuilder: (context, index) {
+            final drink = filteredDrinks[index];
+            return GestureDetector(
+              onTap: () => onDrinkSelected(drink),
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.75,
+                padding: const EdgeInsets.all(5),
+                margin: const EdgeInsets.only(top: 5, left: 10),
+                decoration: BoxDecoration(
+                  color: _getDrinkColor(drink.type),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Row(
+                  children: [
+                    Image.network(drink.imageUrl, width: 40, height: 40, fit: BoxFit.contain),
+                    const SizedBox(width: 10),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(drink.name, style: TextStyle(color: Theme.of(context).colorScheme.onPrimary, fontWeight: FontWeight.w600)),
+                        Row(
+                          children: [
+                            Text('abv:${drink.abv}', style: TextStyle(color: Theme.of(context).colorScheme.onPrimary)),
+                            if (drink.isVegan) const SizedBox(width: 5),
+                            if (drink.isVegan) Text(' *vegan', style: TextStyle(color: Theme.of(context).colorScheme.onPrimary)),
+                            const SizedBox(width: 5),
+                            if (drink.isGlutenFree) Text(' *gf', style: TextStyle(color: Theme.of(context).colorScheme.onPrimary)),
+                            if (drink.isGlutenFree) const SizedBox(width: 5),
+                            if (drink.calories.toString().isNotEmpty) Text(' *${drink.calories.round().toString()}cals', style: TextStyle(color: Theme.of(context).colorScheme.onPrimary)),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-              child: Row(
-                children: [
-                  Image.network(drink.imageUrl, width: 40, height: 40, fit: BoxFit.contain),
-                  const SizedBox(width: 10),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(drink.name, style: TextStyle(color: Theme.of(context).colorScheme.onPrimary, fontWeight: FontWeight.w600)),
-                      Row(
-                        children: [
-                          Text('abv:${drink.abv}', style: TextStyle(color: Theme.of(context).colorScheme.onPrimary)),
-                          if (drink.isVegan) const SizedBox(width: 5),
-                          if (drink.isVegan) Text(' *vegan', style: TextStyle(color: Theme.of(context).colorScheme.onPrimary)),
-                          const SizedBox(width: 5),
-                          if (drink.isGlutenFree) Text(' *gf', style: TextStyle(color: Theme.of(context).colorScheme.onPrimary)),
-                          if (drink.isGlutenFree) const SizedBox(width: 5),
-                          if (drink.calories.toString().isNotEmpty) Text(' *${drink.calories.round().toString()}cals', style: TextStyle(color: Theme.of(context).colorScheme.onPrimary)),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
-    );
+      Container(
+        height: MediaQuery.of(context).size.height * .05,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              gradientColor.withOpacity(0.0), // Transparent at the top
+              gradientColor.withOpacity(0.5), // Semi-transparent
+              gradientColor.withOpacity(1.0), // Fully opaque at the bottom
+            ],
+          ),
+        ),
+      ),
+    ]);
   }
 
   Color _getDrinkColor(String type) {
