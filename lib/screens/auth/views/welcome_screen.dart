183156739af7dd1716c8802722f5a164fc494382
eddie arnold <eddiearnoldz@mac.com>
2024-07-22 14:44:02 +0100

    ios build update

--- lib/screens/auth/views/welcome_screen.dart
+++ lib/screens/auth/views/welcome_screen.dart
@@ -63,7 +63,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> with TickerProviderStateM
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 20),
                   child: Text(
-                    "find and enjoy the best no and low alcohol options in nearby!",
+                    "find and enjoy the best no and low alcohol options in town",
                     style: TextStyle(
                       fontFamily: 'Work Sans',
                       fontSize: 18,
